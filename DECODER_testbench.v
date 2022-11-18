018b0281abc53a44aef839a59433c63fc79b50df
Andy Dung <118319306+andykydung@users.noreply.github.com>
2022-11-17 22:33:27 -0500

    Add files via upload

--- DECODER_testbench.v
+++ DECODER_testbench.v
@@ -29,15 +29,23 @@ wire halt;
 
 
 DECODER dut(
+
 .instruction(instruction),
 .ALU_op_d(ALU_op_d),
 .immsel(immsel),
 .halt(halt)
 );
-
+/*
 initial begin
-instruction = 32'b000000000000_00110_00111_000_00010_0110011;
+clk = 0;
+end
 
+always #5 clk = ~clk;
+*/
+initial begin
+instruction = 32'b000000000000_00110_00111_000_00010_0110011;// this is ADD rd = 00010, rs1 = 00111, rs2 = 00110, so aluop should be 2
+#10 instruction = 32'b0000000000000011000111000_00010_1101111;// this is JAL, aluop should be same = 2
+#10 instruction = 32'b000000000000_00110_00111_000_00010_1100011;// this is BNE
 end
 
 endmodule
